f0d92dc8539853209e1d1dcd7df809d85f542e57
“Pascwin” <pascal-winner@web.de>
2022-08-06 11:39:26 +0200

    created a useHttp Hook to request record card data from firebasedatabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
+import useHttp from './hooks/use-http';
 
 function App() {
+  const [recordCards, setRecordCards] = useState<any>()
+
+  const {error, isLoading, sendRequest} = useHttp()
+
+  const transformRecordCards = useCallback((data: any) => {
+    const transformedRecordCards = [];
+    for (const cardKey in data) {
+      transformedRecordCards.push({
+        id: cardKey,
+        category: data[cardKey].category,
+        stage: data[cardKey].state,
+        question: data[cardKey].question,
+        answer: data[cardKey].answer,
+      })
+    }
+    setRecordCards(transformedRecordCards)
+  }, [])
+
+
+  useEffect(() => {
+    sendRequest({
+      url: 'https://http-record-cards-default-rtdb.europe-west1.firebasedatabase.app/record-cards.json',
+      method: undefined,
+      headers: undefined,
+      body: undefined
+    }, transformRecordCards)
+  }, [transformRecordCards, sendRequest])
+
+  let content;
+
+  if (isLoading) {
+    content = <p>is Loading...</p>
+  }
+
+  if (error) {
+    content = <p>an error occured</p>
+  }
+
+  if (!error && !isLoading) {
+    content =  recordCards?.map((card: any) => {
+      return <p key={card.id}>{card.answer}</p>
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App">{content}</div>
   );
 }
 
---
+++ src/hooks/use-http.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+
+interface RequestObject {
+    url: string,
+    method: string | undefined,
+    headers: {} | undefined,
+    body: string | undefined
+}
+
+const useHttp = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(false)
+
+const sendRequest = async(requestObj: RequestObject, applyData: any) => {
+    try {
+        const response = await fetch(requestObj.url, {
+            method: requestObj.method ? requestObj.method : 'GET',
+            headers: requestObj.headers ? requestObj.headers : {},
+            body: requestObj.body ? JSON.stringify(requestObj.body) : null
+        })
+        if(!response.ok) {
+            throw new Error("something went wrong!")
+        }
+        const data = await response.json()
+        applyData(data)
+    } catch(err) {
+        setError(true)
+        console.log(err)
+    }
+    setIsLoading(false)
+}
+
+return {
+    error,
+    isLoading,
+    sendRequest,
+}
+}
+
+export default useHttp;
