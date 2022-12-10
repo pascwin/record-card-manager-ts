7ac98d03556bd631c3199658ee3a528d0c1a345f
“Pascwin” <pascal-winner@web.de>
2022-12-10 15:13:54 +0100

    when creating a record, add the option to create the same record just with reversed question and answer

--- src/pages/category/AddRecordsModal/AddRecordsModal.tsx
+++ src/pages/category/AddRecordsModal/AddRecordsModal.tsx
@@ -1,9 +1,10 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import Modal from "@mui/material/Modal";
 import { useFirestore } from "../../../hooks/useFirestore";
 import "./AddRecordsModal.scss";
+import { FormControlLabel, Switch } from "@mui/material";
 
 const AddRecordsForm = ({ open, setOpen, uid }: any) => {
   const question = useRef<HTMLInputElement>(null);
@@ -11,6 +12,7 @@ const AddRecordsForm = ({ open, setOpen, uid }: any) => {
   const tip = useRef<HTMLInputElement>(null);
   const { addDocument } = useFirestore("records");
   const location = useLocation();
+  const [reverseChecked, setReverseChecked] = useState<boolean>(false);
 
   const handleOpen = () => {
     setOpen(true);
@@ -19,6 +21,14 @@ const AddRecordsForm = ({ open, setOpen, uid }: any) => {
   const handleClose = () => {
     setOpen(false);
   };
+
+  const handleReverseChecked = () => {
+    setReverseChecked((prevState: boolean) => {
+      return !prevState;
+    });
+  };
+
+  console.log(reverseChecked)
 
   const addRecordHandler = (event: any) => {
     event.preventDefault();
@@ -31,6 +41,17 @@ const AddRecordsForm = ({ open, setOpen, uid }: any) => {
       lastRepeat: null,
       tip: tip.current?.value,
     });
+    if (reverseChecked) {
+      addDocument({
+        uid: uid,
+        category: location.pathname.slice(10),
+        answer: question.current?.value,
+        question: answer.current?.value,
+        stage: 1,
+        lastRepeat: null,
+        tip: tip.current?.value,
+      });
+    }
     event.target.reset();
   };
 
@@ -60,6 +81,17 @@ const AddRecordsForm = ({ open, setOpen, uid }: any) => {
             <br></br>
             <p>add a tip</p>
             <input ref={tip} />
+            <br></br>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={reverseChecked}
+                  onChange={handleReverseChecked}
+                  name="jason"
+                />
+              }
+              label="create reverse Record"
+            />
             <br></br>
             <button>add record</button>
           </form>
